9c3d5639c927ac949462aecd4bcb6317a9c0a38e
Od4ir <od4ir@usp.br>
2022-11-25 22:06:52 -0300

    Organização geral do repositório

--- Pasta/filaPrioridades.h
+++
@@ -1,9 +0,0 @@
-typedef struct {
-    char letras[10];
-} elemento;
-
-void CorrigeHeapDescendo(elemento *v, int n, int i);
-void ConstroiHeap (elemento *v, int n);
-void troca(elemento *v, int i, int j);
-
-
